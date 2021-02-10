9b43027483400baed66011eb6905f9f3631659ac
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-02-10 20:12:55 +0100

    code from the lesson added

--- 100days.playground/Contents.swift
+++ 100days.playground/Contents.swift
@@ -1,3 +1,94 @@
 import UIKit
 
-var str = "Hello, playground"
+// Creating your own structs
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+print(tennis.name)
+
+// Computed properties
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+// Property observers
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progess = Progress(task: "Loading data", amount: 0)
+progess.amount = 10
+progess.amount = 70
+progess.amount = 100
+
+// Methods
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+print(london.collectTaxes())
+
+// Mutating methods
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var mariush = Person(name: "Mariush")
+print(mariush.name)
+
+mariush.makeAnonymous()
+mariush.name
+
+// Properties and methods of strings
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+print(string.replacingOccurrences(of: " ", with: "_"))
+
+// Properties and methods of arrays
+var toys = ["Woody"]
+print(toys.count)
+
+toys.append("Buzz")
+toys.append("Teddy Bear")
+toys.append("Buzz")
+
+print(toys.first)
+print(toys.firstIndex(of: "Buzz"))
+print(toys.count)
+
+print(toys.sorted())
+print(toys.remove(at: 3))
