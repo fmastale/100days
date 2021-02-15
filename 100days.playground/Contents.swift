baa8244046890b46177971452e5c990aa69dd97f
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-02-15 19:07:03 +0100

    added code from lessons

--- 100days.playground/Contents.swift
+++ 100days.playground/Contents.swift
@@ -1,3 +1,102 @@
 import UIKit
 
-var str = "Hello, playground"
+// Protocols
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+let mariush = User(id: "1234")
+
+displayID(thing: mariush)
+
+// Protocol inheritance
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Emoloyee: Payable, NeedsTraining, HasVacation {}
+
+struct GrandParadeEmployee: Emoloyee {
+    func takeVacation(days: Int) {
+        print("Employee has still \(days) of annual leave")
+    }
+    
+    func study() {
+        print("Employee is studying Swift right now!")
+    }
+    
+    func calculateWages() -> Int {
+        return 1_000_000
+    }
+}
+// Extensions
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+let number2 = 9
+number2.isEven
+
+// Protocol extensions
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+// Protocol-oriented programming
+protocol Identifiable2 {
+    var id: String { get set}
+    func identify()
+}
+
+extension Identifiable2 {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User2: Identifiable2{
+    var id: String
+}
+
+let ziom = User2(id: "ziom")
+ziom.identify()
+
