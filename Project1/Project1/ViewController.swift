8b4decab79a5070bf4123e507a377462acdd6d4e
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-02-22 22:14:26 +0100

    created project

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Filip Mastalerz on 22/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
