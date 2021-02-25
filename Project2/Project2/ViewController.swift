6a0f86aea40ade0f261cf66f39ccfd3633bab308
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-02-25 18:22:47 +0100

    added new project

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Filip Mastalerz on 25/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
