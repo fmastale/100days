5592794cb0a3ca3a9da7972f321a53d1f223473d
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-03 20:55:24 +0100

    Added proj files

---
+++ MilestoneProj1/MilestoneProj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProj1
+//
+//  Created by Filip Mastalerz on 03/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
