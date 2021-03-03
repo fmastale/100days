01779af874c865f1531e1f66daac06a3e874801e
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-03 21:56:13 +0100

    Added possibility to load flags names into the table view

--- MilestoneProj1/MilestoneProj1/ViewController.swift
+++ MilestoneProj1/MilestoneProj1/ViewController.swift
@@ -22,9 +22,17 @@ class ViewController: UITableViewController {
                 flags.append(item)
             }
         }
-        print(flags)
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        
+        return cell
+    }
 }
 
