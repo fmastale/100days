476e22830627096572eea2a278e300d0e8c88613
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-03 21:43:01 +0100

    Added flags to content, content loading and table view controller

--- MilestoneProj1/MilestoneProj1/ViewController.swift
+++ MilestoneProj1/MilestoneProj1/ViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var flags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("@2x.png") {
+                flags.append(item)
+            }
+        }
+        print(flags)
     }
 
 
