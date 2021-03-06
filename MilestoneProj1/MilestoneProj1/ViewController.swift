d6898f1807d6837e8e6a2b66ae175257de8fe196
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-06 19:18:00 +0100

    Tavle view now showing country names instead of filenames

--- MilestoneProj1/MilestoneProj1/ViewController.swift
+++ MilestoneProj1/MilestoneProj1/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UITableViewController {
         
         for item in items {
             if item.hasSuffix("@2x.png") {
-                flags.append(item)
+                let flagName = item.deleteSuffix("@2x.png")
+                
+                flags.append(flagName)
             }
         }
     }
@@ -30,9 +32,23 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
-        cell.textLabel?.text = flags[indexPath.row]
+        
+        cell.textLabel?.text = uppercase(flags[indexPath.row].capitalized)
         
         return cell
     }
 }
 
+extension String {
+    func deleteSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+private func uppercase(_ country: String) -> String {
+    if country == "Uk" || country == "Us" {
+        return country.uppercased()
+    }
+    return country
+}
