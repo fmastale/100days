cec475fe3589eb334483001513b5aa27ecada2fe
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-06 21:13:48 +0100

    Added sharing option

--- MilestoneProj1/MilestoneProj1/DetailViewController.swift
+++ MilestoneProj1/MilestoneProj1/DetailViewController.swift
@@ -14,12 +14,22 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             // how to add border around flag image
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
         
-        
-        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
