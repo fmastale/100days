b36cfab4a6373a05edc5cc77b98ac0dd2b12d469
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-16 21:46:04 +0100

    Added code to support adding groceries

--- GroceryList/GroceryList/ViewController.swift
+++ GroceryList/GroceryList/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var products = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Grocery list"
     }
 
+    func cleanList() {
+        products.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Product", for: indexPath)
+        cell.textLabel?.text = products[indexPath.row]
+        return cell
+    }
 
 }
 
