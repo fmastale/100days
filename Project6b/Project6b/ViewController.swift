f8452294e6be02c1ce56fd112a7fa92411ae69cc
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-16 20:23:17 +0100

    Missing code

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -48,6 +48,16 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight@999)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//
         var previous: UILabel?
         
         for label in  [label1, label2, label3, label4, label5] {
