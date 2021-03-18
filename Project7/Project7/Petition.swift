565c6371e42e5dbf1aabf86f6e567213ce08ab1f
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-18 22:12:43 +0100

    Added possibility to load data from json

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Filip Mastalerz on 18/03/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Filip Mastalerz on 18/03/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
