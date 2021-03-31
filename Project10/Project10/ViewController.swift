3aec4ed56bad487ab8a9974a36a79e51dc5e7d98
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-31 21:16:45 +0200

    Added changes for second lesson

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
 }
 
