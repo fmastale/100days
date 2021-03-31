4f0146931ad909c9751f8acc1e08d6fb6a9201a2
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-03-31 20:41:45 +0200

    Added changes for first lesson

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Filip Mastalerz on 31/03/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
