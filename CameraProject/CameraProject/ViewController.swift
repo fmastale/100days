e62cce87e790b091e6fb86cc54be9f9a22de1260
Filip Mastalerz <Filip.Mastalerz@grandparade.co.uk>
2021-04-21 21:23:50 +0200

    Added new project

---
+++ CameraProject/CameraProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraProject
+//
+//  Created by Filip Mastalerz on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
